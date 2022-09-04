08581281bef0691006fa39aabce2d65da3b95a5e
execute <dimon321853@mail.ru>
2022-09-04 07:50:54 +0300

    Добавлены циклы и условия

--- JSproject/js/main.js
+++ JSproject/js/main.js
@@ -1,9 +1,6 @@
 "use strict";
 const number0fFilms = +prompt('Сколько фильмов вы уже посомтрели ?', '');
-const lastFilms = prompt('Один из просмотренных фильмов?', ''),
-      ratingFilms = +prompt ('На сколько оцените его? ( По десятибальной шкале )',''),
-      secondLastFilms = prompt('Один из просмотренных фильмов?', ''),
-      secondRatingFilms = +prompt ('На сколько оцените его? ( По десятибальной шкале )','');
+
 let personalMovieDB = {
     count: number0fFilms,
     movies: {},
@@ -12,7 +9,30 @@ let personalMovieDB = {
     private: false
 };
 
-personalMovieDB.movies[lastFilms] = ratingFilms;
-personalMovieDB.movies [secondLastFilms] = secondRatingFilms;
+for (let i = 0; i < 2; i++){
+    const lastFilms = prompt('Один из просмотренных фильмов?', ''),
+          ratingFilms = +prompt ('На сколько оцените его?','');
+    
+    if (lastFilms != '' && ratingFilms != '' && 
+    lastFilms.length < 50 && lastFilms != null && ratingFilms != null) {
+        
+        personalMovieDB.movies[lastFilms] = ratingFilms;
+        console.log ('done');
+    } else {
+        console.log ('error');
+        i--;
+    }
+
+}
+
+if ( personalMovieDB.count < 10) {
+    console.log ('Просмотренно мало фильмов');
+} else if ( personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log ('Вы классический зритель');
+} else if ( personalMovieDB.count >= 30) {
+    console.log ('Вы киноман !');
+} else {
+    console.log ('error');
+}
 
 console.log (personalMovieDB);
